9c122f433316af04e89347e61d3c281d6190395a
mohith <mohithkumar808@gmail.com>
2023-12-27 00:08:59 +0530

    made model images load at once

--- components/CanvasAnimation.jsx
+++ components/CanvasAnimation.jsx
@@ -1,153 +1,78 @@
-// // components/CanvasAnimation.js
-// import React, { useRef, useEffect } from "react";
-// import gsap from "gsap";
-// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
-// const CanvasAnimation = () => {
-//   const canvasRef = useRef(null);
-//   const imageSeq = { frame: 1 };
-
-//   useEffect(() => {
-//     gsap.registerPlugin(ScrollTrigger);
-
-//     const canvas = canvasRef.current;
-//     const context = canvas.getContext("2d");
-//     const frameCount = 359; // Adjust this according to your image count
-
-//     const images = Array.from({ length: frameCount }, (_, i) => {
-//       const img = new Image();
-//       img.src = `/3dPerson/cam.${String(i + 1).padStart(4, "0")}.png`;
-//       return img;
-//     });
-//     console.log(images);
-
-//     gsap.to(imageSeq, {
-//       frame: frameCount - 1,
-//       snap: "frame",
-//       ease: "none",
-//       scrollTrigger: {
-//         scrub: 0.15,
-//         trigger: canvas,
-//         start: "top top",
-//         end: "600% top",
-//       },
-//       onUpdate: render,
-//     });
-
-//     images[1].onload = render;
-
-//     function render() {
-//       // Clear canvas before rendering
-//       context.clearRect(0, 0, canvas.width, canvas.height);
-
-//       // Scale and draw the current image
-//       scaleImage(images[imageSeq.frame], context);
-//     }
-
-//     function scaleImage(img, ctx) {
-//       // Calculate the aspect ratio to fit the image within the canvas
-//       const aspectRatio = img.width / img.height;
-//       const canvasWidth = canvas.width;
-//       const canvasHeight = canvas.width / aspectRatio;
-
-//       // Draw the image on the canvas
-//       ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight);
-//     }
-
-//     return () => {
-//       // Cleanup logic if needed
-//     };
-//   }, []);
-
-//   return (
-//     <canvas
-//       ref={canvasRef}
-//       className="fixed w-full h-screen z-20 bottom-0 left-0"
-//     />
-//   );
-// };
-
-// export default CanvasAnimation;
-
-// components/CanvasAnimation.js
-
 import React, { useRef, useEffect, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import Image from "next/image";
 
 const CanvasAnimation = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
   const canvasRef = useRef(null);
   const frameCount = 359; // Adjust this according to your image count
 
   useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
+    const preloadedImages = [];
 
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
+    const loadImages = async () => {
+      try {
+        for (let i = 1; i <= frameCount; i++) {
+          const img = new Image();
+          img.src = `/3dPerson/cam.${String(i).padStart(4, "0")}.png`;
+          await new Promise((resolve, reject) => {
+            img.onload = resolve;
+            img.onerror = reject;
+          });
+          preloadedImages.push(img);
+        }
 
-    ScrollTrigger.create({
-      trigger: canvas,
-      start: "top top",
-      end: "+=600%",
-      scrub: 0.15,
-      onUpdate: (self) => {
-        const newFrame = Math.min(
-          Math.floor(self.progress * (frameCount - 1)),
-          frameCount - 1
-        );
-        setCurrentFrame(newFrame);
-      },
-    });
+        setImagesLoaded(true);
+      } catch (error) {
+        console.error("Error loading images:", error);
+      }
+    };
+
+    loadImages();
 
     return () => {
       // Cleanup logic if needed
     };
-  }, []);
+  }, [frameCount]);
 
   useEffect(() => {
-    // console.log(currentFrame);
-    // const canvas = canvasRef.current;
-    // const context = canvas.getContext("2d");
-    // const renderImage = () => {
-    //   const imgSrc = `/3dPerson/cam.${String(currentFrame + 1).padStart(
-    //     4,
-    //     "0"
-    //   )}.png`;
-    //   const img = new Image();
-    //   img.src = imgSrc;
-    //   img.onload = () => {
-    //     const aspectRatio = img.width / img.height;
-    //     const canvasWidth = canvas.width;
-    //     const canvasHeight = canvasWidth / aspectRatio;
-    //     context.clearRect(0, 0, canvas.width, canvas.height);
-    //     context.drawImage(img, 0, 0, canvasWidth, canvasHeight);
-    //   };
-    // };
-    // renderImage();
-  }, [currentFrame]);
+    if (imagesLoaded) {
+      gsap.registerPlugin(ScrollTrigger);
+
+      const canvas = canvasRef.current;
+
+      ScrollTrigger.create({
+        trigger: canvas,
+        start: "top top",
+        end: "+=600%",
+        scrub: 0.15,
+        onUpdate: (self) => {
+          const newFrame = Math.min(
+            Math.floor(self.progress * (frameCount - 1)),
+            frameCount - 1
+          );
+          setCurrentFrame(newFrame);
+        },
+      });
+    }
+  }, [imagesLoaded, frameCount]);
+
+  if (!imagesLoaded) {
+    return <div>Loading...</div>; // Placeholder until all images are loaded
+  }
 
   return (
-    <>
-      <div className="fixed w-full h-screen z-20 bottom-0 left-0 ">
-        <canvas ref={canvasRef} className="w-full h-full" />
-        <div className="w-full  h-screen ">
-          <Image
-            src={`/3dPerson/cam.${String(currentFrame + 1).padStart(
-              4,
-              "0"
-            )}.png`}
-            // layout="fill"
-            // objectFit="cover"
-            width={1000}
-            height={1000}
-            alt="3D Person"
-            className="  object-cover max-w-[100vw] ov fixed w-full h-screen z-20 -bottom-44 md:bottom-0 left-0"
-          />
-        </div>
+    <div className="fixed w-full h-screen z-20 bottom-0 left-0 ">
+      <canvas ref={canvasRef} className="w-full h-full" />
+      <div className="w-full h-screen" style={{ position: "relative" }}>
+        <img
+          src={`/3dPerson/cam.${String(currentFrame + 1).padStart(4, "0")}.png`}
+          alt="3D Person"
+          className="object-cover max-w-[100vw] ov fixed w-full h-screen z-20 -bottom-44 md:bottom-0 left-0"
+        />
       </div>
-    </>
+    </div>
   );
 };
 
