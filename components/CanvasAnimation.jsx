083a217d4ad7687b18d4e4f20161fafc991a80c9
mohith <mohithkumar808@gmail.com>
2023-12-27 00:29:13 +0530

    made model images load at once

--- components/CanvasAnimation.jsx
+++ components/CanvasAnimation.jsx
@@ -1,39 +1,65 @@
 import React, { useRef, useEffect, useState } from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import Image from "next/image";
 
 const CanvasAnimation = () => {
   const [currentFrame, setCurrentFrame] = useState(0);
   const [imagesLoaded, setImagesLoaded] = useState(false);
+  const [loadedCount, setLoadedCount] = useState(20); // Initial loaded count
   const canvasRef = useRef(null);
-  const frameCount = 359; // Adjust this according to your image count
+  const frameCount = 358; // Total image count
 
   useEffect(() => {
-    const loadImages = async () => {
+    const loadInitialImages = async () => {
       try {
         const imagePromises = [];
-        for (let i = 1; i <= frameCount; i++) {
-          const img = new Image();
-          img.src = `/3dPerson/cam.${String(i).padStart(4, "0")}.png`;
-          const promise = new Promise((resolve, reject) => {
+        for (let i = 1; i <= loadedCount; i++) {
+          const promise = new Promise((resolve) => {
+            const img = document.createElement("img");
             img.onload = resolve;
-            img.onerror = reject;
+            img.src = `/3dPerson/cam.${String(i).padStart(4, "0")}.png`;
           });
           imagePromises.push(promise);
         }
         await Promise.all(imagePromises);
         setImagesLoaded(true);
       } catch (error) {
-        console.error("Error loading images:", error);
+        console.error("Error loading initial images:", error);
       }
     };
 
-    loadImages();
+    loadInitialImages();
 
     return () => {
       // Cleanup logic if needed
     };
-  }, [frameCount]);
+  }, [loadedCount]);
+
+  useEffect(() => {
+    if (imagesLoaded && loadedCount < frameCount) {
+      const loadRemainingImages = async () => {
+        try {
+          const remainingImages = [];
+          for (let i = loadedCount + 1; i <= frameCount; i++) {
+            const promise = new Promise((resolve) => {
+              const img = document.createElement("img");
+              img.onload = resolve;
+              img.src = `/3dPerson/cam.${String(i).padStart(4, "0")}.png`;
+            });
+            remainingImages.push(promise);
+          }
+          await Promise.all(remainingImages);
+          setImagesLoaded(true);
+        } catch (error) {
+          console.error("Error loading remaining images:", error);
+        }
+      };
+
+      loadRemainingImages();
+      setLoadedCount(frameCount); // Update loaded count to all images
+    }
+  }, [imagesLoaded, frameCount, loadedCount]);
 
   useEffect(() => {
     if (imagesLoaded) {
@@ -65,9 +91,11 @@ const CanvasAnimation = () => {
     <div className="fixed w-full h-screen z-20 bottom-0 left-0 ">
       <canvas ref={canvasRef} className="w-full h-full" />
       <div className="w-full h-screen" style={{ position: "relative" }}>
-        <img
+        <Image
           src={`/3dPerson/cam.${String(currentFrame + 1).padStart(4, "0")}.png`}
           alt="3D Person"
+          width={1000}
+          height={1000}
           className="object-cover max-w-[100vw] ov fixed w-full h-screen z-20 -bottom-44 md:bottom-0 left-0"
         />
       </div>
