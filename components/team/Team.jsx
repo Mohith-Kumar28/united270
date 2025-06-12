3c8cc32d551699b14a8ab585bccf6103daca7b1f
mohith-kumar28 <mohithkumar808@gmail.com>
2025-06-13 00:01:17 +0530

    added new team member details and updated existing member information

--- components/team/Team.jsx
+++ components/team/Team.jsx
@@ -28,6 +28,16 @@ const Team = () => {
       name: "~SENAPATHI KESAVA",
       role: "Chief Operations Officer & Co- Founder",
       photo: "/images/team/person5.png",
+      photoSmall: "/images/team/5.png",
+      brands: "",
+      linkedinUrl: "https://www.linkedin.com/in/kesava-senapathi/",
+      description:
+        "With over 22 years of expertise in the IT domain across diverse business verticals, Senapathi is our experienced advisor and mentor. His multi-faceted expertise lies in areas like IT Strategy & Planning, Enterprise Solution Design, and Business Transformation - including Digital Transformation ranging from Big data and Robotics to Apps and Al.",
+    },
+    {
+      name: "~Kiran",
+      role: "Brand Producer & Core Team",
+      photo: "/images/team/person5.png",
       photoSmall: "/images/team/4.png",
       brands: "",
       linkedinUrl: "https://www.linkedin.com/in/kesava-senapathi/",
